eac8961b4dbb1d914c3e001fb713662a49166ef6
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2023-09-10 07:39:10 +0530

    Added solution - LeetHub

--- Count pairs with given sum - GFG/count-pairs-with-given-sum.cpp
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.cpp
@@ -21,17 +21,13 @@ public:
             int num1 = p.first;
             int freq1 = p.second;
             int num2 = k - num1;
+            int freq2 = hash[num2];
             
-            if(num1==num2){
+            if(num1==num2)
                 ans += (freq1)*(freq1-1) / 2;
-            }
-            else{
-                if(hash.find(num2) != hash.end()){
-                    int freq2 = hash[num2];
-                    ans += freq1*freq2;
-                }
-            }
-            hash.erase(num1);
+            else
+                ans += freq1*freq2;
+            hash[num1] = 0;
         }
         
         return ans;
