3ac9da75d323f6b7367f21153965d5d4353e198a
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2023-11-16 14:58:19 +0530

    Added solution - LeetHub

--- Subarrays with sum K - GFG/subarrays-with-sum-k.cpp
+++ Subarrays with sum K - GFG/subarrays-with-sum-k.cpp
@@ -13,13 +13,11 @@ class Solution{
         
         int prefixSum = 0;
         unordered_map<int,int>freq;
+        freq[0] = 1;
         int ans = 0;
         
         for(int i=0; i<n; i++){
             prefixSum += arr[i];
-            
-            if(prefixSum==k)
-                ans++;
             
             int remainSum = prefixSum - k;
             
