914f5e19af9ae60e175904081a38d1790086bffe
Ujjwal Maheshwari <ujjwal2327@gmail.com>
2023-12-30 08:01:44 +0530

    Added solution - LeetHub

--- N meetings in one room - GFG/n-meetings-in-one-room.cpp
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -3,50 +3,40 @@
 using namespace std;
 
 // } Driver Code Ends
-
-struct meeting {
-   int start;
-   int end;
-   int pos;
-};
-
 class Solution
 {
-    
-    
-    bool static comparator(struct meeting m1, meeting m2) {
-        if (m1.end < m2.end) return true;
-        else if (m1.end > m2.end) return false;
-        else if (m1.pos < m2.pos) return true;
-        return false;
+    bool static custom_comparator(pair<int,int>a, pair<int,int>b){
+        if(a.second==b.second)
+            return a.first<b.first;
+        return a.second<b.second;
     }
     
     public:
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
-    int maxMeetings(int s[], int e[], int n)
+    int maxMeetings(int start[], int end[], int n)
     {
         // Your code here
-        struct meeting meet[n];
-        for (int i = 0; i < n; i++) {
-            meet[i].start = s[i], meet[i].end = e[i], meet[i].pos = i + 1;
-        }
-
-        sort(meet, meet + n, comparator);
-
-        int ans=1;
-        int limit = meet[0].end;
-
-        for (int i = 1; i < n; i++) {
-            if (meet[i].start > limit) {
-                limit = meet[i].end;
-                ans++;
-            }
+        vector<pair<int,int>>time(n);
+        for(int i=0; i<n; i++){
+            time[i].first = start[i];
+            time[i].second = end[i];
         }
         
+        sort(time.begin(),time.end(), custom_comparator);
+        
+        int ans = 0, lastEnd=-1;
+        for(auto p : time){
+            if(p.first>lastEnd){
+                ans++;
+                lastEnd = p.second;
+            }
+        }
+
         return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
